c430d505d3cead08aceadfda4d97e21e553f4ae4
itsjohnye <itsjohnye@gmail.com>
2021-01-29 23:39:02 +0800

    First mommit

---
+++ lesson23/gps.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type gps struct {
+	world
+	currentLocation location
+	targetLocation  location
+}
+type name string
+type location struct {
+	name
+	lat, long float64
+}
+type world struct {
+	radius float64
+}
+
+//rad函数会将角度转换为弧度
+func rad(deg float64) float64 {
+	return deg * math.Pi / 180
+}
+
+//distance使用余弦球面定律计算两个位置之间的距离
+func (w world) distance(p1, p2 location) float64 {
+	s1, c1 := math.Sincos(rad(p1.lat))
+	s2, c2 := math.Sincos(rad(p2.lat))
+	clong := math.Cos(rad(p1.long - p2.long))
+	return w.radius * math.Acos(s1*s2+c1*c2*clong)
+}
+
+func (l location) description() string {
+	return fmt.Sprintf("%v (%.2fº, %.2fº)", l.name, l.lat, l.long)
+}
+
+func (g gps) distance() float64 {
+	return g.world.distance(g.currentLocation, g.targetLocation)
+}
+func (g gps) message() string {
+	return fmt.Sprintf("%v to %v is about %.2f KMs", g.currentLocation.description(), g.targetLocation.description(), g.distance())
+}
+
+type rover struct {
+	gps
+}
+
+func main() {
+
+	mars := world{radius: 3389.5}
+	bradbury := location{"Bradbury Landing", -4.5895, 137.4417}
+	elysium := location{"Elysium Planitia", 4.5, 135.9}
+
+	gpsInMars := gps{
+		world:           mars,
+		currentLocation: bradbury,
+		targetLocation:  elysium,
+	}
+
+	curiosity := rover{
+		gps: gpsInMars,
+	}
+
+	fmt.Println(curiosity.message())
+
+}
---
+++ lesson24/marshal.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type coordinate struct {
+	d, m, s float64
+	h       rune
+}
+
+func (c coordinate) decimal() float64 {
+	sign := 1.0
+	switch c.h {
+	case 'S', 'W', 's', 'w':
+		sign = -1
+	}
+	return sign * (c.d + c.m/60 + c.s/3600)
+}
+
+// String formats a DMS coordinate.
+func (c coordinate) String() string {
+	return fmt.Sprintf("%vº%v'%.1f\" %c", c.d, c.m, c.s, c.h)
+}
+
+type location struct {
+	Name string     `JSON:"name"` //记得大写
+	Lat  coordinate `JSON:"latitude"`
+	Long coordinate `JSON:"longtitude"`
+}
+
+func (c coordinate) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct { //用Marshal序列化，Json的key键值是个struct结构
+		DD  float64 `json:"decimal"`
+		DMS string  `json:"dms"`
+		D   float64 `json:"degrees"`
+		M   float64 `json:"minutes"`
+		S   float64 `json:"seconds"`
+		H   string  `json:"hemisphere"`
+	}{
+		DD:  c.decimal(), //与key对应的value值
+		DMS: c.String(),
+		D:   c.d,
+		M:   c.m,
+		S:   c.s,
+		H:   string(c.h),
+	})
+}
+
+func main() {
+	elysium := location{
+		Name: "Elysium Planitia",
+		Lat:  coordinate{4, 30, 0.0, 'N'},
+		Long: coordinate{135, 54, 0.0, 'E'},
+	}
+
+	//因为coordinate满足了MarshalJSON() ([]byte, error)隐式接口，所以可以直接被json.Marshal调用
+	message, err := json.MarshalIndent(elysium, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(message)) //message是[]byte类型，转化成string类型便于查看
+}
