01203982f73bd2cf7b9d4f8a4486c817fb7a2cc3
itsjohnye <itsjohnye@gmail.com>
2021-03-26 17:03:20 +0800

    c

---
+++ The Go Programming Language/ch8/countdown/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func launch() {
+	fmt.Println("Launched!")
+}
+
+func main() {
+	fmt.Println("Commecing countdown.")
+	tick := time.Tick(1 * time.Second) //time.Tick函数返回一个通道
+
+	abort := make(chan struct{})
+	go func() {
+		os.Stdin.Read(make([]byte, 1)) //读取单个字节
+		abort <- struct{}{}
+	}()
+
+	for countdown := 10; countdown > 0; countdown-- {
+		fmt.Println(countdown)
+		select {
+		case <-tick:
+			//do not thing
+		case <-abort:
+			fmt.Println("Launch aborted!")
+			return
+		}
+
+	}
+	launch()
+}
